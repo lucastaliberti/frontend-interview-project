e0417e458003f9b577a06cc086df7ed3dd97a97c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-10 16:34:28 -0200

    Testing cases for every day of the week, just need to work on friday, I should return an empty array

--- src/availabilities/getAvailabilities.test.js
+++ src/availabilities/getAvailabilities.test.js
@@ -19,14 +19,14 @@ describe("#getAvailabilities", () => {
 
   it("consider breaks", () => {
     expect(getAvailabilities(OFFICE_HOURS["monday"], [])).toEqual([
-    {
+      {
         startTime: "2018-02-01T08:00:00.000Z",
         endTime: "2018-02-01T12:00:00.000Z"
-    },
-    {
+      },
+      {
         startTime: "2018-02-01T13:00:00.000Z",
         endTime: "2018-02-01T17:00:00.000Z"
-    }
+      }
     ]);
   });
 
@@ -35,18 +35,18 @@ describe("#getAvailabilities", () => {
     expect(
       getAvailabilities(OFFICE_HOURS[testingDay], APPOINTMENTS[testingDay])
     ).toEqual([
-    {
+      {
         startTime: "2018-02-01T08:00:00.000Z",
         endTime: "2018-02-01T09:30:00.000Z"
-    },
-    {
+      },
+      {
         startTime: "2018-02-01T11:30:00.000Z",
         endTime: "2018-02-01T12:00:00.000Z"
-    },
-    {
+      },
+      {
         startTime: "2018-02-01T14:00:00.000Z",
         endTime: "2018-02-01T17:00:00.000Z"
-    }
+      }
     ]);
   });
 
@@ -55,11 +55,11 @@ describe("#getAvailabilities", () => {
     expect(
       getAvailabilities(OFFICE_HOURS[testingDay], APPOINTMENTS[testingDay])
     ).toEqual([
-    {
+      {
         startTime: "2018-02-01T13:30:00.000Z",
         endTime: "2018-02-01T14:30:00.000Z"
-    },
-    {
+      },
+      {
         startTime: "2018-02-01T15:30:00.000Z",
         endTime: "2018-02-01T18:30:00.000Z"
       }
@@ -71,16 +71,12 @@ describe("#getAvailabilities", () => {
     expect(
       getAvailabilities(OFFICE_HOURS[testingDay], APPOINTMENTS[testingDay])
     ).toEqual([
-    {
-        startTime: "2018-02-01T08:00:00.000Z",
-        endTime: "2018-02-01T09:30:00.000Z"
-    },
-    {
-        startTime: "2018-02-01T11:30:00.000Z",
+      {
+        startTime: "2018-02-01T10:00:00.000Z",
         endTime: "2018-02-01T12:00:00.000Z"
-    },
-    {
-        startTime: "2018-02-01T14:00:00.000Z",
+      },
+      {
+        startTime: "2018-02-01T15:00:00.000Z",
         endTime: "2018-02-01T17:00:00.000Z"
       }
     ]);
@@ -93,43 +89,19 @@ describe("#getAvailabilities", () => {
     ).toEqual([
       {
         startTime: "2018-02-01T08:00:00.000Z",
-        endTime: "2018-02-01T09:30:00.000Z"
-  },
-      {
-        startTime: "2018-02-01T11:30:00.000Z",
-        endTime: "2018-02-01T12:00:00.000Z"
-  },
-      {
-        startTime: "2018-02-01T14:00:00.000Z",
-        endTime: "2018-02-01T17:00:00.000Z"
-      }
-
-describe("#getAvailabilities", () => {
-  it("is a function", () => {
-    expect(typeof getAvailabilities).toBe("function");
-  });
-
-  it("returns empty when is closed", () => {
-    expect(
-      getAvailabilities(OFFICE_HOURS["sunday"], APPOINTMENTS["sunday"]).length
-    ).toBe(0);
-
-    expect(
-      getAvailabilities(OFFICE_HOURS["saturday"], APPOINTMENTS["saturday"])
-        .length
-    ).toBe(0);
-  });
-
-  it("consider breaks", () => {
-    expect(getAvailabilities(OFFICE_HOURS["monday"], [])).toEqual([
-      {
-        startTime: "2018-02-01T08:00:00.000Z",
         endTime: "2018-02-01T12:00:00.000Z"
       },
       {
-        startTime: "2018-02-01T13:00:00.000Z",
-        endTime: "2018-02-01T17:00:00.000Z"
+        startTime: "2018-02-01T15:00:00.000Z",
+        endTime: "2018-02-01T18:00:00.000Z"
       }
     ]);
+  });
+
+  it("works on friday", () => {
+    let testingDay = "friday";
+    expect(
+      getAvailabilities(OFFICE_HOURS[testingDay], APPOINTMENTS[testingDay])
+    ).toEqual([]);
   });
 });
