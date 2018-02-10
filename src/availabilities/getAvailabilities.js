1b084cc0512903698f47d903dcbf0d7986296590
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-10 16:15:15 -0200

    Done with the function to get the availabilities, based on joining everything that breaks the office hours together so I can iterate it all at once. I had the reducer and the sorting on separate functions for better readability.

--- src/availabilities/getAvailabilities.js
+++ src/availabilities/getAvailabilities.js
@@ -1,5 +1,4 @@
-
-import moment from 'moment';
+import moment from "moment";
 
 /**
  * getAvailabilities is function that will compute the open time-slots based on officeHours and appointments
@@ -7,8 +6,53 @@ import moment from 'moment';
  * @returns {[availabilities]}
  */
 export default function getAvailabilities(officeHours, appointments) {
-  return [
-    { startDate: moment().toISOString(), endDate: moment().toISOString() },
-    { startDate: moment().toISOString(), endDate: moment().toISOString() },
-  ];
+  if (officeHours.isClosed) return [];
+  else {
+    const notAvailableTimes = appointments
+      .concat(officeHours.breaks)
+      .sort(sortByStartTime);
+
+    const availableOfficeHours = notAvailableTimes.reduce(
+      officeHoursBreaksReducer(officeHours),
+      { hours: [] }
+    ).hours;
+
+    return availableOfficeHours.sort(sortByStartTime);
+  }
 }
+
+const sortByStartTime = (a, b) => new Date(a.startTime) - new Date(b.startTime);
+
+const officeHoursBreaksReducer = officeHours => (acc, breaks, index, arr) => {
+  const startTime = acc.nextStartTime
+    ? acc.nextStartTime
+    : officeHours.startTime;
+
+  const breakStart = moment
+    .utc(breaks.startTime)
+    .diff(moment.utc(startTime), "m");
+  const breakDuration = moment
+    .utc(breaks.endTime)
+    .diff(moment.utc(breaks.startTime), "m");
+
+  const startTimePlusBreak = moment.utc(startTime).add(breakStart, "m");
+
+  acc.nextStartTime = startTimePlusBreak
+    .clone()
+    .add(breakDuration, "m")
+    .toISOString();
+
+  if (index == arr.length - 1)
+    acc.hours.push({
+      startTime: acc.nextStartTime,
+      endTime: officeHours.endTime
+    });
+
+  if (breakStart > 0)
+    acc.hours.push({
+      startTime,
+      endTime: startTimePlusBreak.toISOString()
+    });
+
+  return acc;
+};
