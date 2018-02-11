8e713b4be686b93426c5b9380a72869f5ff4cec0
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-11 19:39:20 -0200

    Added transitions for the data loading

--- src/modules/Popup/containers/PopupWindow.js
+++ src/modules/Popup/containers/PopupWindow.js
@@ -1,8 +1,8 @@
 import React from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 import PopupWindowHead from "../components/PopupWindowHead";
 import Calendar from "../components/Calendar";
-import TimeSlotCard from "../components/TimeSlotCard";
 
 import "./PopupWindow.css";
 
@@ -11,9 +11,13 @@ const PopupWindow = ({ toggleOpen, items, selectedDay, handleDayClick }) => (
     <PopupWindowHead toggleOpen={toggleOpen} />
     <Calendar selectedDay={selectedDay} handleDayClick={handleDayClick} />
     <div className="popup-window_body">
-      <TimeSlotCard startTime="8:30" endTime="9:30" />
-      <TimeSlotCard startTime="13:30" endTime="15:30" />
-      <TimeSlotCard startTime="17:30" endTime="19:30" />
+      <ReactCSSTransitionGroup
+        transitionName="popup-window_card_transition"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        {items}
+      </ReactCSSTransitionGroup>
     </div>
   </div>
 );
