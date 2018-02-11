a90a9bde4ccfa7d4c47559317930eb86121dd56c
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-11 20:07:40 -0200

    Reworked the Card component to be more semantic

---
+++ src/modules/Popup/components/NotAvailableCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FontAwesome from "react-fontawesome";
+
+import Card from "./Card";
+
+const NotAvailableCard = () => <Card text={"no times available..."} />;
+
+export default NotAvailableCard;
--- src/modules/Popup/components/TimeSlotCard.js
+++ src/modules/Popup/components/TimeSlotCard.js
@@ -1,15 +1,10 @@
 import React from "react";
 import FontAwesome from "react-fontawesome";
 
-import "./TimeSlotCard.css";
+import Card from "./Card";
 
 const TimeSlotCard = ({ startTime, endTime }) => (
-  <div className="popup-window_card">
-    <div className="popup-window_card-line">
-      <FontAwesome name="angle-right" size="2x" tag="i" />
-    </div>
-    <div className="popup-window_card-content">{`from ${startTime} to ${endTime}`}</div>
-  </div>
+  <Card text={`from ${startTime} to ${endTime}`} />
 );
 
 export default TimeSlotCard;
