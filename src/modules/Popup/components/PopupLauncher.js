c4b5747369b895fbed8d0abf2fa9fb6a76394cc9
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-11 21:34:01 -0200

    Migrated plain css to aphrodite

--- src/modules/Popup/components/PopupLauncher.js
+++ src/modules/Popup/components/PopupLauncher.js
@@ -1,29 +1,69 @@
 import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
 import FontAwesome from "react-fontawesome";
 
-import "./PopupLauncher.css";
-
 const PopupLauncher = ({ isOpen, toggleOpen }) => (
-  <div className="popup-launcher" onClick={toggleOpen}>
-    <div
-      className={`popup-launcher_wrapper ${
-        isOpen ? "popup-launcher_active" : ""
-      }`}
-    >
-      <FontAwesome
-        name="calendar"
-        tag="i"
-        size="2x"
-        className="popup-launcher_open"
-      />
-      <FontAwesome
-        name="plus"
-        tag="i"
-        size="2x"
-        className="popup-launcher_close"
-      />
-    </div>
+  <div className={css(styles.popup_launcher)} onClick={toggleOpen}>
+    <FontAwesome
+      name="calendar"
+      tag="i"
+      size="2x"
+      className={css(
+        styles.popup_icon,
+        styles.popup_launcher_open,
+        isOpen && styles.popup_launcher_open_active
+      )}
+    />
+    <FontAwesome
+      name="plus"
+      tag="i"
+      size="2x"
+      className={css(
+        styles.popup_icon,
+        styles.popup_launcher_close,
+        isOpen && styles.popup_launcher_close_active
+      )}
+    />
   </div>
 );
+
+const styles = StyleSheet.create({
+  popup_launcher: {
+    boxShadow: "0 2px 8px rgba(0, 0, 0, 0.09), 0 4px 40px rgba(0, 0, 0, 0.24)",
+    position: "fixed",
+    bottom: "20px",
+    right: "20px",
+    width: "60px",
+    height: "60px",
+    borderRadius: "50%",
+    background: "#ff715a",
+    cursor: "pointer"
+  },
+  popup_icon: {
+    position: "absolute",
+    lineHeight: "1em",
+    color: "white",
+    transition: "transform 0.16s linear, opacity 0.08s linear"
+  },
+  popup_launcher_open: {
+    opacity: 1,
+    top: "14px",
+    left: "16px"
+  },
+  popup_launcher_open_active: {
+    opacity: 0,
+    transform: "rotate(30deg) scale(0)"
+  },
+  popup_launcher_close: {
+    opacity: 0,
+    top: "15px",
+    left: "17px"
+  },
+  popup_launcher_close_active: {
+    opacity: 1,
+    transform: "rotate(45deg)"
+  }
+});
 
 export default PopupLauncher;
