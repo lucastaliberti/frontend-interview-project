2c4bd81de31f2b51b8ae41e045a4e1591fa3e3ee
Lucas Taliberti de Sousa <lucastaliberti@syllus.com.br>
2018-02-11 21:36:50 -0200

    css -> aphrodite

--- src/modules/Popup/components/Calendar.js
+++ src/modules/Popup/components/Calendar.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { StyleSheet, css } from "aphrodite";
 import DayPicker from "react-day-picker";
 import "react-day-picker/lib/style.css";
 
 import "./Calendar.css";
 
+const modifiersStyles = {
+  selected: {
+    backgroundColor: "#206477"
+  },
+  today: {
+    color: "#ff715a"
+  }
+};
+
 const Calendar = ({ selectedDay, handleDayClick }) => (
-  <div className="popup-window_calendar">
-    <DayPicker selectedDays={selectedDay} onDayClick={handleDayClick} />
+  <div className={css(styles.popup_window_calendar)}>
+    <DayPicker
+      selectedDays={selectedDay}
+      onDayClick={handleDayClick}
+      modifiersStyles={modifiersStyles}
+    />
   </div>
 );
+
+const styles = StyleSheet.create({
+  popup_window_calendar: {
+    boxShadow: "0 1px 4px rgba(0, 0, 0, 0.2)",
+    fontSize: "0.8em"
+  }
+});
 
 export default Calendar;
